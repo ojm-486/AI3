e0ddfb4f1e13f1a3b131768acd3a377af4b432ef
ojm-486 <h098s02812@chunchon.gwe.hs.kr>
2025-11-11 14:21:14 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,7 +70,7 @@ st.markdown("---")
 CONTENT_BY_LABEL: dict[str, dict[str, list[str]]] = {
     
      labels[0]: {
-       "texts": ["윤석열은 병신이다."],
+       "texts": ["박정희는 애미가 없다."],
     #   "images": ["https://.../jjampong1.jpg", "https://.../jjampong2.jpg"],
     #   "videos": ["https://youtu.be/XXXXXXXXXXX"]
      },
